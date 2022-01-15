51f6a955e607870e3fd9abe742fdd328f8afd150
talgat.aimbetov.b@gmail.com <84536810+Frataim@users.noreply.github.com>
2022-01-15 12:37:40 +0300

    Main page and Signup

--- client/src/components/Authentication/Signup/Signup.jsx
+++ client/src/components/Authentication/Signup/Signup.jsx
@@ -1,40 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import {useDispatch, useSelector} from 'react-redux'
-import {useNavigate} from 'react-router-dom'
-import { addUser } from '../../../redux/actions/userAction';
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import style from './style.module.css'
+import { addUser } from '../../../redux/actions/userAction'
 
 // зарегистрироваться
 const Signup = () => {
   const [reg, setReg] = useState({}) // состояние регистрации
   const navigate = useNavigate()
   const dispatch = useDispatch() // чтоб изменить состояние внутри компонента получаем диспатч
-  const user = useSelector((state) => state.user) // получаем состояние 
+  const user = useSelector((state) => state.user) // получаем состояние
 
-// если пользователь прошел регистрацию то мы его направляем на его заметки
-useEffect(() => {
-  if (user) {
+  // если пользователь прошел регистрацию то мы его направляем на его заметки
+  useEffect(() => {
+    if (user) {
+      navigate('/')
+    }
+  }, [user])
+
+  const inputChange = (e) => {
+    setReg((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  }
+
+  const regHandler = (e) => {
+    e.preventDefault()
+    dispatch(addUser(reg))
     navigate('/')
   }
-}, [user])
-
-
-const inputChange = (e) => {
-  setReg((prev) => ({...prev,[e.target.name] : e.target.value}))
-}
-
-const regHandler = (e) => {
-  e.preventDefault()
-  dispatch(addUser(reg))
-  navigate('/')
-
-
-}
   return (
-    <form onSubmit={(e) => regHandler(e)}>
-    <div className="container my-3">
-        <label htmlFor="exampleInputText" className="form-label">
-          Name
-        </label>
+    <form className={style.form} onSubmit={(e) => regHandler(e)}>
+      <div className={style.container}>
+        <h3 className={style.h3}>Имя</h3>
         <input
           type="text"
           name="name"
@@ -45,10 +41,8 @@ const regHandler = (e) => {
           aria-describedby="emailHelp"
         />
       </div>
-      <div className="container my-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email address
-        </label>
+      <div className={style.container}>
+        <h3>Адрес электронной почты</h3>
         <input
           type="email"
           name="email"
@@ -59,10 +53,8 @@ const regHandler = (e) => {
           aria-describedby="emailHelp"
         />
       </div>
-      <div className="container mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Password
-        </label>
+      <div className={style.container}>
+        <h3>Пароль</h3>
         <input
           type="password"
           name="password"
@@ -72,11 +64,17 @@ const regHandler = (e) => {
           id="exampleInputPassword1"
         />
       </div>
-    <div className="container mb-3">
-    <button type="submit" className="btn btn-success">Registration</button>
-    </div>
-  </form>
-  );
+      <div className={style.container}>
+        <button
+          className={style.button}
+          type="submit"
+          className="btn btn-success"
+        >
+          Поехали
+        </button>
+      </div>
+    </form>
+  )
 }
 
-export default Signup;
+export default Signup
