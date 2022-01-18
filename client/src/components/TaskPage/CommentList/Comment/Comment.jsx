63a75218794fd87f926ffd891658b0e360d9ace9
IlyaRogovenko <hello@librory.org>
2022-01-18 10:04:53 +0300

    мэтчинг почти доделал

--- client/src/components/TaskPage/CommentList/Comment/Comment.jsx
+++ client/src/components/TaskPage/CommentList/Comment/Comment.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { upTaskThunk } from '../../../../redux/actions/tasksAc';
 import style from './style.module.css'
 
 function Comment( { comment, user_id, task_id, User }) {
 
   const dispatch = useDispatch()
+
+  const user = useSelector((state) => state.user)
+  const tasks = useSelector((state) => state.tasks)
+  const task = tasks.filter(el => el.id === task_id)[0]
+
+  console.log('TASK ------------>', User);
 
   const handleMatch = () => {
     console.log('CLICK', User, comment, user_id, task_id);
@@ -21,9 +27,13 @@ function Comment( { comment, user_id, task_id, User }) {
       <div>
         {comment}
       </div>
-      <button type='button' onClick={handleMatch} className={style.btn}>
-        ВЫБРАТЬ ИСПОЛНИТЕЛЯ
-      </button>
+      {
+        user.role === 2 && task.owner === user.id && (
+          <button type='button' onClick={handleMatch} className={style.btn}>
+            ВЫБРАТЬ ИСПОЛНИТЕЛЯ
+          </button>
+        )
+      }
     </div>
   )
 }
