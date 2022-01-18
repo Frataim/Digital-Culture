efe167ecada5b6705a76b839386e4218dbb7f766
IlyaRogovenko <hello@librory.org>
2022-01-18 18:51:21 +0300

    chat

--- server/db/seeders/4-task.js
+++ server/db/seeders/4-task.js
@@ -2,10 +2,10 @@ module.exports = {
   up: async (queryInterface) => {
     await queryInterface.bulkInsert('Tasks', [
       {
-        title: 'Exhibition in London',
+        title: 'Настройка CRM для Океанического музея',
         category: 1,
-        description: 'Help to create a new website for exhibition in London',
-        deadline: '2013-10-27 02:00:00',
+        description: 'Океаническому музею г. Калининград нужна помощь с&nbsp;переносом файлов из&nbsp;нашей crm &laquo;Планфикс&raquo; в&nbsp;наше облако (Облако тоже надо настроить). Необходимо знание API запросов и&nbsp;специфики облака. С&nbsp;нашей стороны специалист по&nbsp;crm настроит сценарии, а&nbsp;вам нужно будет сделать API запросы на&nbsp;перенос файлов, чтобы в&nbsp;crm оставалась только ссылка на&nbsp;файл, а&nbsp;сам файл уже хранился в&nbsp;облаке. Наш специалист по&nbsp;crm будет вам помогать на&nbsp;протяжении всей задачи. Фонд готов на&nbsp;основании договора между вами и&nbsp;фондом заплатить небольшое вознаграждение за&nbsp;выполнение данной задачи. Размер оплаты по&nbsp;договорённости.',
+        deadline: '2022-02-14 02:00:00',
         isDone: false,
         status: 1,
         owner: 2,
