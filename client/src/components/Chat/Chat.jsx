fec6bad0d5ae8bb28a6de4c7c562f0b377142731
IvanWolve <lysenkov.orel@gmail.com>
2022-01-19 11:06:48 +0300

    allPull

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -6,14 +6,16 @@ const Chat = () => {
   const [input, setInput] = useState("")
   useEffect(() => {
     socket.onopen = () => {
-      console.log("sadas", input) 
+      console.log("sssss", input)
+      socket.send(JSON.stringify({ id: '2', msg: input }))
     }
   }, [input])
   const handleClick = (e) => {
     e.preventDefault()
     setInput(e.currentTarget.mess.value)
     console.log(e.currentTarget.mess.value)
-    socket.send(JSON.stringify({id:'2', msg: input }))
+
+    e.currentTarget.mess.value = ''
   }
 
   return (
